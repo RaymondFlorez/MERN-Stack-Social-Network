5e7726bc7980bb1afd37f02bc6d34a41477460cc
Ray Flores <rlf2128@gmail.com>
2019-01-13 16:41:29 -0600

    MERN Social Network Files excluding node modules
    Node modules not included because too many files for GitHub.

---
+++ build/precache-manifest.93e8a2699c8baff075302f8c76997d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46de45c8de569623eda8e63160d519db",
+    "url": "/static/media/showcase.46de45c8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7f2f00540db2725efaf",
+    "url": "/static/js/main.a7f2f005.chunk.js"
+  },
+  {
+    "revision": "9509ed34a6dbe8779ee9",
+    "url": "/static/js/1.9509ed34.chunk.js"
+  },
+  {
+    "revision": "a7f2f00540db2725efaf",
+    "url": "/static/css/main.d034fbd9.chunk.css"
+  },
+  {
+    "revision": "f8f080e0cb1a282dc7704dd5ae231845",
+    "url": "/index.html"
+  }
+];
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://ray:1e24epkr@ds151943.mlab.com:51943/mernsocialnetwork",
+  secretOrKey: "secret"
+};
